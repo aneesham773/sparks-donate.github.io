27fb96971a2e6295e351419456e56caeeb42eb36
aneesham773 <61931107+aneesham773@users.noreply.github.com>
2021-03-11 13:56:48 +0530

    Update configDb.php

--- configDb.php
+++ configDb.php
@@ -1,9 +1,12 @@
-<?php 
-$db = mysqli_connect('localhost:3307', 'root', '', 'razorpay'); 
-if(mysqli_connect_errno()){
-	echo "Failed to connect to MYSQL:".mysqli_connect_error();
-	exit();
-}
- 
+<?php
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server=$cleardb_url["host"];
+$cleardb_username=$cleardb_url["user"];
+$cleardb_password=$cleardb_url["pass"];
+$cleardb_db=substr($cleardb_url["path"],1);
+$active_group='default';
+$query_builder = TRUE;
+$con= mysqli_connect($cleardb_server,$cleardb_username,$cleardb_password,$cleardb_db);
+
 
 ?>
